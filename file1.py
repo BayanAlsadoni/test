1afad0b7a0d92aa5c2cf8448509124c4af131382
BayanAlsadoni <bayanabd2002@gmail.com>
2022-09-19 20:51:59 +0300

    iii

--- file1.py
+++ file1.py
@@ -4,10 +4,12 @@ discount_rate= float(input('enter the percentage discount🔗'))
 discount= price * (discount_rate/ 100.0)
 discount_price= price- discount
 
+
 print('price before discount= £', price)
 print('discount rate= %', discount_rate)
 print('discount= £', discount)
 print('price after discount= £', discount_price)
+
 
 
 
--- file2.py
+++ file2.py
@@ -6,7 +6,6 @@ lower= 0
 upper= 0
 punctuation=0
 
-
 if len(password)>=8:
     password.strip()
     for i in password:
@@ -22,12 +21,9 @@ if len(password)>=8:
                 lower+=1
 
 
-
 if num>0 and upper>0 and lower>0 and punctuation>0:
     print('your password is achieve the all items👍🏻')
 else:
     print('your password is incorrect maybe your lose some items😅', num, upper,lower,punctuation)
-
-
 
 
